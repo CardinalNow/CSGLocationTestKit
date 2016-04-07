6ac930524ab908d5fc92dde8930cdc22ec606b0f
Rachel Parsons <vc.rachelanne.parsons@lowes.com>
2016-04-07 13:30:09 -0400

    code cleanup; added circle overlay over radius of region being monitored

--- CSGLocationTestKit/CSGMapViewController.m
+++ CSGLocationTestKit/CSGMapViewController.m
@@ -26,17 +26,10 @@
     [super viewDidLoad];
 	self.locationManager = [CSGLocationManager sharedInstance];
     
-    MKCoordinateSpan span = MKCoordinateSpanMake(.1, .1);
-    CLLocationCoordinate2D centeringCoord = self.locationManager.currentLocation.coordinate;
-    self.mapView.region = MKCoordinateRegionMake(centeringCoord, span);
-	
-	self.coordinate = CLLocationCoordinate2DMake(40.819204, -96.698171);
-	
-	MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
-	[annotation setCoordinate:self.coordinate];
-	[annotation setTitle:@"Central Point of Region Monitoring"]; //You can set the subtitle too
-	[self.mapView addAnnotation:annotation];
-	
+    [self zoomInOnCoordinate];
+    [self initiateCoordinate];
+    [self dropPinForCoordinate];
+    [self dropCircleAroundPin];
 }
 
 - (void)didReceiveMemoryWarning
@@ -44,8 +37,34 @@
     [super didReceiveMemoryWarning];
     [[NSNotificationCenter defaultCenter] removeObserver:self name:USER_ENTERED_REGION object:nil];
 	[[NSNotificationCenter defaultCenter] removeObserver:self name:USER_LEFT_REGION object:nil];
-
 }
+
+#pragma mark - Map build out methods
+
+-(void)zoomInOnCoordinate{
+    MKCoordinateSpan span = MKCoordinateSpanMake(.1, .1);
+    CLLocationCoordinate2D centeringCoord = self.locationManager.currentLocation.coordinate;
+    self.mapView.region = MKCoordinateRegionMake(centeringCoord, span);
+}
+
+-(void)initiateCoordinate{
+    self.coordinate = CLLocationCoordinate2DMake(40.819204, -96.698171);
+    [self.mapView setCenterCoordinate:self.coordinate];
+}
+
+-(void)dropPinForCoordinate{
+    MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
+    [annotation setCoordinate:self.coordinate];
+    [annotation setTitle:@"Central Point of Region Monitoring"]; //You can set the subtitle too
+    [self.mapView addAnnotation:annotation];
+}
+
+-(void)dropCircleAroundPin{
+    MKCircle *circle = [MKCircle circleWithCenterCoordinate:self.coordinate radius:500];
+    [self.mapView addOverlay:circle];
+}
+
+#pragma mark - Notification handling
 
 -(void)userEnteredRegion:(NSNotification*)notification{
     self.almostThereView.text = @"You're approaching the area";
@@ -65,6 +84,18 @@
 	}];
 	
 }
+
+#pragma mark - MapView delegate
+
+- (MKOverlayRenderer*)mapView:(MKMapView *)mapView rendererForOverlay:(id<MKOverlay>)overlay{
+    MKCircleRenderer *circleRenderer = [[MKCircleRenderer alloc] initWithOverlay:overlay];
+    circleRenderer.strokeColor = [UIColor blueColor];
+    circleRenderer.fillColor = [UIColor colorWithRed:0.0 green:0.0 blue:255.0/255.0 alpha:.1];
+    circleRenderer.lineWidth = 1;
+    return circleRenderer;
+}
+
+#pragma mark - Button action
 
 -(IBAction)watchButtonDidTouchUpInside:(id)sender{
     self.almostThereView.text = @"watching!";
